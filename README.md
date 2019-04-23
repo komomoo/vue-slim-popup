c4151df71a8b880c1913f17ea6e17f84a38fe870
momoko <ko.momo@qq.com>
2019-04-23 17:24:12 +0800

    feat: props popupPosition 支持更多位置： 'center', 'top', 'bottom', 'left', 'right'

--- README.md
+++ README.md
@@ -47,12 +47,12 @@ export default {
 | hideOnMaskClick | 点击遮罩是否关闭弹窗 | Boolean | false |
 | forceRenderOnShow | 显示的时候是否重新渲染 | Boolean | false |
 | maskTransition | 遮罩动画 | String | slim-fade |
-| popupTransition | 弹窗动画，内置 'slim-scale', 'slim-zoom', 'slim-fade-in-bottom', 'slim-slide-in-bottom' | String | slim-scale |
+| popupTransition | 弹窗动画，内置 'slim-scale-center', 'slim-zoom-center', 'slim-slide-top', 'slim-slide-bottom', 'slim-slide-left', 'slim-slide-right'。为空则根据 popupPosition 选用默认动画 | String | null |
 | maskClass | 遮罩的样式类 | Array | null |
 | popupClass | 弹窗的样式类 | Array | null |
-| maskStyle | 遮罩的样式 | Object | null |
-| popupStyle | 弹窗的样式 | Object | null |
-| popupPosition | 弹窗的位置，可选 'center', 'top', 'bottom' | String | center |
+| maskStyle | 遮罩样式 | Object | null |
+| popupStyle | 弹窗样式 | Object | null |
+| popupPosition | 弹窗位置，可选 'center', 'top', 'bottom', 'left', 'right' | String | center |
 | preventMaskTouchmove | 阻止遮罩 touchmove 事件，阻止移动端滚动穿透 | Boolean | true |
 | preventPopupTouchmove | 阻止弹窗 touchmove 事件，阻止移动端滚动穿透（同时会导致弹窗区域无法滚动） | Boolean | true |
 | preventBodyScroll | 阻止 body 滚动，以间接的阻止滚动穿透（不会影响弹窗区域滚动）。开启此选项，关闭 preventPopupTouchmove，可达到弹窗区域可滚动，同时阻止滚动穿透的效果 | Boolean | false |
@@ -60,24 +60,6 @@ export default {
 <br>
 <br>
 😉😘 如果它对你有所帮助，可以点一下 <b>⭐️<a href="#">Star</a></b> ~
-
-## 📝 更新日志
-
-### v1.3.2
-
-* 增加 props forceRenderOnShow
-
-### v1.3.0
-
-* 更好的滚动穿透控制。增加 props preventMaskTouchmove & preventPopupTouchmove & preventBodyScroll
-
-### v1.2.0
-
-* props popupClass & maskClass 类型由 String 变为 Array
-
-### v1.1.0
-
-* 移除 slim-radius 动画
 
 ## License
 
--- demo/App.vue
+++ demo/App.vue
@@ -3,67 +3,49 @@
     <h1>中间弹出</h1>
 
     <div class="btn-box">
-      <button
-        v-for="(item, index) in 4"
-        :key="index"
-        class="open-btn"
-        @click="popupShow(index)">
-        Click Me
-      </button>
+      <button @click="centerShow = true"> Scale </button>
+      <button @click="centerShow2 = true"> Zoom </button>
     </div>
 
     <SlimPopup
-      :show.sync="show"
-      :popup-transition="popupTransition"
+      :show.sync="centerShow"
       :popup-class="['popup']">
       <h2 style="color: #6D7A80;">这里是一个插槽，可以放置任何元素</h2>
-      <button class="close-btn" @click="popupHide">X</button>
+      <button class="close-btn" @click="centerShow = false">X</button>
     </SlimPopup>
-
-    <h1>底部弹出</h1>
-
-    <div class="btn-box">
-      <button class="open-btn" @click="show2 = true">Click Me</button>
-    </div>
-
     <SlimPopup
-      :show.sync="show2"
-      :popup-class="['popup']"
-      popup-transition="slim-slide-in-bottom"
-      popup-position="bottom">
+      :show.sync="centerShow2"
+      popup-transition="slim-zoom-center"
+      :popup-class="['popup']">
       <h2 style="color: #6D7A80;">这里是一个插槽，可以放置任何元素</h2>
-      <button class="close-btn" @click="show2 = false">X</button>
+      <button class="close-btn" @click="centerShow2 = false">X</button>
     </SlimPopup>
 
-    <h1>关闭阻止滚动穿透</h1>
+    <h1>弹出位置</h1>
 
     <div class="btn-box">
-      <button class="open-btn" @click="show3 = true">Click Me</button>
+      <button @click="()=>{position = 'top'; positionShow = true}"> Top </button>
+      <button @click="()=>{position = 'bottom'; positionShow = true}"> Bottom </button>
+      <button @click="()=>{position = 'left'; positionShow = true}"> Left </button>
+      <button @click="()=>{position = 'right'; positionShow = true}"> Right </button>
     </div>
 
     <SlimPopup
-      :show.sync="show3"
-      :prevent-popup-touchmove="false"
-      :popup-class="['popup scroll']">
-      <h2 style="color: #6D7A80;">滚动元素</h2>
-      <h2 style="color: #6D7A80;">滚动元素</h2>
-      <h2 style="color: #6D7A80;">滚动元素</h2>
-      <h2 style="color: #6D7A80;">滚动元素</h2>
-      <h2 style="color: #6D7A80;">滚动元素</h2>
-      <h2 style="color: #6D7A80;">滚动元素</h2>
-      <h2 style="color: #6D7A80;">滚动元素</h2>
-      <h2 style="color: #6D7A80;">滚动元素</h2>
-      <button class="close-btn" @click="show3 = false">X</button>
+      :show.sync="positionShow"
+      :popup-class="['popup']"
+      :popup-position="position">
+      <h2 style="color: #6D7A80;">这里是一个插槽，可以放置任何元素</h2>
+      <button class="close-btn" @click="positionShow = false">X</button>
     </SlimPopup>
 
     <h1>开启阻止 body 滚动</h1>
 
     <div class="btn-box">
-      <button class="open-btn" @click="show4 = true">Click Me</button>
+      <button class="open-btn" @click="scrollShow = true">Click Me</button>
     </div>
 
     <SlimPopup
-      :show.sync="show4"
+      :show.sync="scrollShow"
       :prevent-popup-touchmove="false"
       :prevent-body-scroll="true"
       :popup-class="['popup scroll']">
@@ -75,7 +57,7 @@
       <h2 style="color: #6D7A80;">滚动元素</h2>
       <h2 style="color: #6D7A80;">滚动元素</h2>
       <h2 style="color: #6D7A80;">滚动元素</h2>
-      <button class="close-btn" @click="show4 = false">X</button>
+      <button class="close-btn" @click="scrollShow = false">X</button>
     </SlimPopup>
   </div>
 </template>
@@ -90,100 +72,89 @@ export default {
   },
   data () {
     return {
-      show: false,
-      show2: false,
-      show3: false,
-      show4: false,
-      popupTransition: '',
+      centerShow: false,
+      centerShow2: false,
+      positionShow: false,
+      position: 'top',
+      scrollShow: false,
     }
-  },
-  created () {
-    this.transitionList = ['slim-scale', 'slim-zoom', 'slim-fade-in-bottom', 'slim-slide-in-bottom']
-  },
-  methods: {
-    // 显示
-    popupShow (index) {
-      this.popupTransition = this.transitionList[index] // 更换动效
-      this.show = true
-    },
-    // 隐藏
-    popupHide () {
-      this.show = false
-    },
   },
 }
 </script>
 
 <style lang="stylus">
-$headerHeight = 44px;
-$baseColor = #6A9FB5;
-$bgColor = #FAFAFA;
+$headerHeight = 44px
+$baseColor = #6A9FB5
+$bgColor = #FAFAFA
 * {
-  padding: 0;
-  margin: 0;
-  border: none;
+  padding 0
+  margin 0
+  border none
 }
 html, body {
-  width: 100%;
-  height: 100%;
-  overflow: hidden;
+  width 100%
+  height 100%
+  overflow hidden
 }
 #app {
-  width: 100%;
-  height: 100%;
-  overflow: hidden;
-  overflow-y: scroll;
-  box-sizing: border-box;
+  width 100%
+  height 100%
+  overflow hidden
+  overflow-y scroll
+  box-sizing border-box
 
   > h1 {
-    font-size: 24px;
-    padding: 0 15px;
-    padding-top: 40px;
-    padding-bottom: 20px;
-    color: #6D7A80;
+    font-size 24px
+    padding 0 15px
+    padding-top 40px
+    padding-bottom 20px
+    color #6D7A80
   }
 
   .popup {
-    width: 80%;
-    height: 200px;
-    border-radius: 16px;
-    text-align: center;
-    background: #fff;
-    display: flex;
-    align-items: center;
-    flex-direction: column;
-    justify-content: space-around;
+    width 80%
+    height 200px
+    border-radius 16px
+    text-align center
+    background #fff
+    display flex
+    align-items center
+    flex-direction column
+    justify-content space-around
 
     &.scroll {
-      overflow-y: scroll;
-      -webkit-overflow-scrolling: touch;
+      overflow-y scroll
+      -webkit-overflow-scrolling touch
     }
 
     .close-btn {
-      font-size: 20px;
-      font-weight: 500;
-      color: $baseColor;
-      background-color: $bgColor;
-      text-align: center;
-      width: 50px;
-      height: 50px;
-      border-radius: 4px;
+      font-size 20px
+      font-weight 500
+      color $baseColor
+      background-color $bgColor
+      text-align center
+      width 50px
+      height 50px
+      border-radius 4px
     }
   }
 
   .btn-box {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
+    display flex
+    flex-wrap wrap
+    justify-content space-around
+    padding 0 20px
 
-    .open-btn {
-      display: block;
-      width: 60%;
-      height: 100px;
-      margin: 15px auto;
-      font-size: 24px;
-      color: $baseColor;
-      background-color: $bgColor;
+    > button {
+      display block
+      width 140px
+      height 80px
+      font-size 20px
+      font-weight 500
+      color $baseColor
+      background-color $bgColor
+      border 1px solid $borderColor
+      margin-bottom 20px
     }
   }
 }
