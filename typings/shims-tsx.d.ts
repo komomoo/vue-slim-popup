d37e52a07fb0851f91026700ffc15909dca16789
momoko <ko.momo@qq.com>
2019-05-22 15:41:37 +0800

    feat: d.ts

---
+++ typings/shims-tsx.d.ts
@@ -0,0 +1,17 @@
+/**
+ * 在 ts 中支持 jsx 语法
+*/
+
+import Vue, { VNode } from 'vue'
+
+declare global {
+  namespace JSX {
+    // tslint:disable no-empty-interface
+    interface Element extends VNode {}
+    // tslint:disable no-empty-interface
+    interface ElementClass extends Vue {}
+    interface IntrinsicElements {
+      [elem: string]: any
+    }
+  }
+}
--- typings/shims-vue.d.ts
+++ typings/shims-vue.d.ts
@@ -1,3 +1,9 @@
+/**
+ * TypeScript 并不支持 Vue 文件，
+ * 该文件告诉 ts 导入.vue 文件都按VueConstructor<Vue>处理，
+ * 因此导入 vue 文件必须写.vue 后缀
+ */
+
 declare module '*.vue' {
   import Vue from 'vue'
   export default Vue
